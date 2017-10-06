6523c109c055ac41af884b41e34fe65b8fbe90e4
Tom Archer <tomm.archer@gmail.com>
2017-10-06 12:58:17 +0100

    Create a setup script for Python 2.7

---
+++ setup2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "Setting up Masterclass Raspberry Pi."
+
+echo "Downloading Python Puzzles Client Software (for Python 2.7)"
+mkdir /home/pi/Python
+cd /home/pi/Python
+git clone https://github.com/Tom-Archer/python-puzzles-client.git
+cd /home/pi/Python/python-puzzles-client/library
+sudo python2.7 setup.py install
+sudo rm -rf /home/pi/Python/python-puzzles-client/
